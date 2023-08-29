585266013dc1dc4aa75aad908c29e2dff0b95862
jairc <jairc@JHAIR>
2023-08-28 23:28:18 -0500

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Por favor ingrese el primer número:");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Por favor ingrese el segundo número:");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine("Por favor ingrese el tercer número:");
+int C = int.Parse(Console.ReadLine());
+int mayor = A;
+if (B > mayor)
+{
+    mayor = B;
+}
+if (C > mayor)
+{
+    mayor = C;
+}
+Console.WriteLine($"El mayor de los tres números es: {mayor}");
